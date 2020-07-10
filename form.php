391672da3849a84652869aff2f6561495ae15c8c
Roman Ildjanov <r.ildjanov@yandex.com>
2020-07-10 17:03:07 +0300

    123

--- form.php
+++ form.php
@@ -1,10 +1,20 @@
 <?php
-
+/*
 $name = $_FILES['image']['name'];
 $tmp_name = $_FILES['image']['tmp_name'];
 
 move_uploaded_file($tmp_name, "uploads/" . $name);
+*/
+
+function uploadImage(){
+  $name = $_FILES['123']['name'];
+  $tmp_name = $_FILES['123']['tmp_name'];
+
+  move_uploaded_file($tmp_name, "uploads/" . $name);
+}
+
+uploadImage($_FILES['123']);
 
 ?>
 
-<img src="/uploads/zahawi3.jpg" alt="">
+<img src="/uploads/123.jpg" alt="">
