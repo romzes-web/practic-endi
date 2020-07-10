adef8d6aac9f9b98681c334c2f2085da2c266dfa
Roman Ildjanov <r.ildjanov@yandex.com>
2020-07-10 16:56:30 +0300

    asd

--- form.php
+++ form.php
@@ -1,3 +1,10 @@
 <?php
-var_dump($_FILES);
+
+$name = $_FILES['image']['name'];
+$tmp_name = $_FILES['image']['tmp_name'];
+
+move_uploaded_file($tmp_name, "uploads/" . $name);
+
 ?>
+
+<img src="/uploads/zahawi3.jpg" alt="">
